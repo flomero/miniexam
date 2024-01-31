03a31d40b949cb0abe18807dd0976029dd234240
jojomo96 <48690914+jojomo96@users.noreply.github.com>
2024-01-31 21:11:04 +0100

    Add test files and update exercise description

---
+++ .tests/ex01/test01.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+source .tests/compile_and_test.sh
+
+# Setup variables
+EXERCISE_DIR="code/ex01"
+EXERCISE_FILE="ex01.c"
+MAIN_FILE=".tests/ex01/main.c"
+EXECUTABLE="test_executable"
+EXPECTED_FILE=".tests/ex01/01.expected"
+OUTPUT_FILE="output.tmp"
+TEST_INPUTS=("Hello" "." "")
+CURRENT_EXERCISE_NUMBER="01"
+NEXT_EXERCISE_NUMBER="02"
+
+# Call functions
+compile_exercise "$EXERCISE_DIR" "$EXERCISE_FILE" "$MAIN_FILE" "$EXECUTABLE"
+run_tests "$EXECUTABLE" "$OUTPUT_FILE" "${TEST_INPUTS[@]}"
+validate_output "$EXPECTED_FILE" "$OUTPUT_FILE" "$CURRENT_EXERCISE_NUMBER"
+prepare_next_exercise "$NEXT_EXERCISE_NUMBER"
+cleanup "$EXECUTABLE" "$OUTPUT_FILE"
