a5c8290205e7ea3e269d9e0b283d8d4ce96a55f7
flomero <49640251+flomero@users.noreply.github.com>
2024-01-31 22:20:46 +0100

    ex06 test

--- .tests/ex06/main.c
+++ .tests/ex06/main.c
@@ -6,16 +6,21 @@
 /*   By: flfische <flfische@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/31 19:56:03 by flfische          #+#    #+#             */
-/*   Updated: 2024/01/31 19:56:13 by flfische         ###   ########.fr       */
+/*   Updated: 2024/01/31 22:18:08 by flfische         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-void	replace_vowels(char *str);
+#include <stdio.h>
+
+char	*replace_vowels(char *str);
 
 int	main(int argc, char **argv)
 {
+	char	*str;
+
 	if (argc != 2)
 		return (0);
-	replace_vowels(argv[1]);
+	str = replace_vowels(argv[1]);
+	printf("%s\n", str);
 	return (0);
 }
