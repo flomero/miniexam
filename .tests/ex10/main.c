1ed42519c3103f5d1c29a45cec52b948391ccb0c
flomero <49640251+flomero@users.noreply.github.com>
2024-02-01 11:16:51 +0100

    ex10 test wip

--- .tests/ex10/main.c
+++ .tests/ex10/main.c
@@ -6,20 +6,50 @@
 /*   By: flfische <flfische@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/31 20:05:15 by flfische          #+#    #+#             */
-/*   Updated: 2024/01/31 20:09:36 by flfische         ###   ########.fr       */
+/*   Updated: 2024/02/01 11:16:20 by flfische         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-void	print_odd(int *arr, int size);
+#include <stdio.h>
+#include <stdlib.h>
+
+void	copy_even(int *arr, int size, int **copy);
 
 int	main(void)
 {
 	int	arr1[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
 	int	arr2[20] = {-1, -2, -3, -4, -5, -6, -7, -8, -9};
+	int	arr3[20];
+	int	**copy;
 
-	print_odd(arr1, 10);
-	print_odd(arr2, 20);
-	print_odd(arr1, 0);
-	print_odd(arr2, -10);
+	copy = malloc(sizeof(int *) * 1);
+	copy_even(arr1, 10, copy);
+	while (*copy != NULL)
+	{
+		printf("%d ", **copy);
+		*copy++;
+	}
+	printf("\n");
+	copy_even(arr2, 20, *copy);
+	while (*copy != NULL)
+	{
+		printf("%d ", **copy);
+		copy++;
+	}
+	printf("\n");
+	copy_even(arr3, 20, *copy);
+	while (*copy != NULL)
+	{
+		printf("%d ", **copy);
+		*copy++;
+	}
+	printf("\n");
+	// free stuff in copy
+	while (copy != NULL)
+	{
+		free(*copy);
+		copy++;
+	}
+	free(copy);
 	return (0);
 }
