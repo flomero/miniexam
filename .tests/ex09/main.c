db68329d9f1ab6acc431285d90cc329612dfcd74
flomero <49640251+flomero@users.noreply.github.com>
2024-02-01 11:02:31 +0100

    update ex09 test

--- .tests/ex09/main.c
+++ .tests/ex09/main.c
@@ -6,7 +6,7 @@
 /*   By: flfische <flfische@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/31 20:03:31 by flfische          #+#    #+#             */
-/*   Updated: 2024/02/01 09:16:31 by flfische         ###   ########.fr       */
+/*   Updated: 2024/02/01 11:00:22 by flfische         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,10 +17,14 @@ char	*str_swap_halves(char *str);
 int	main(int argc, char **argv)
 {
 	char	*str;
+	char	*addr;
 
+	addr = argv[1];
 	if (argc != 2)
 		return (0);
 	str = str_swap_halves(argv[1]);
+	if (str != addr)
+		printf("Wrong Return Value \n");
 	printf("%s\n", str);
 	return (0);
 }
