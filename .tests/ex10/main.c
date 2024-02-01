101303f8603bc9813ce43f20e729a9532ee3d3ef
flomero <49640251+flomero@users.noreply.github.com>
2024-02-01 12:06:13 +0100

    ex10 test

--- .tests/ex10/main.c
+++ .tests/ex10/main.c
@@ -6,50 +6,76 @@
 /*   By: flfische <flfische@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/31 20:05:15 by flfische          #+#    #+#             */
-/*   Updated: 2024/02/01 11:16:20 by flfische         ###   ########.fr       */
+/*   Updated: 2024/02/01 12:04:27 by flfische         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include <stdlib.h>
 
-void	copy_even(int *arr, int size, int **copy);
+int	*copy_even(int *arr, int size);
 
 int	main(void)
 {
-	int	arr1[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
-	int	arr2[20] = {-1, -2, -3, -4, -5, -6, -7, -8, -9};
-	int	arr3[20];
-	int	**copy;
+	int	arr1[] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+	int	size1;
+	int	*copy1;
+	int	arr2[] = {-1, -2, -3, -4, -5, -6, -7, -8, -9};
+	int	size2;
+	int	*copy2;
+	int	*arr3;
+	int	*copy3;
 
-	copy = malloc(sizeof(int *) * 1);
-	copy_even(arr1, 10, copy);
-	while (*copy != NULL)
+	// Test 1: Even-sized array
+	size1 = sizeof(arr1) / sizeof(arr1[0]);
+	copy1 = copy_even(arr1, size1);
+	if (copy1 != NULL)
 	{
-		printf("%d ", **copy);
-		*copy++;
+		for (int i = 0; copy1[i] != 0; ++i)
+		{
+			printf("%d ", copy1[i]);
+		}
+		printf("\n");
+		// Free the allocated memory
+		free(copy1);
 	}
-	printf("\n");
-	copy_even(arr2, 20, *copy);
-	while (*copy != NULL)
+	else
 	{
-		printf("%d ", **copy);
-		copy++;
+		printf("Test 1 failed: Memory allocation error\n");
 	}
-	printf("\n");
-	copy_even(arr3, 20, *copy);
-	while (*copy != NULL)
+	// Test 2: Odd-sized array
+	size2 = sizeof(arr2) / sizeof(arr2[0]);
+	copy2 = copy_even(arr2, size2);
+	if (copy2 != NULL)
 	{
-		printf("%d ", **copy);
-		*copy++;
+		for (int i = 0; copy2[i] != 0; ++i)
+		{
+			printf("%d ", copy2[i]);
+		}
+		printf("\n");
+		// Free the allocated memory
+		free(copy2);
 	}
-	printf("\n");
-	// free stuff in copy
-	while (copy != NULL)
+	else
 	{
-		free(*copy);
-		copy++;
+		printf("Test 2 failed: Memory allocation error\n");
 	}
-	free(copy);
+	// Test 3: Empty array
+	arr3 = NULL;
+	copy3 = copy_even(arr3, 0);
+	if (copy3 != NULL)
+	{
+		for (int i = 0; copy3[i] != 0; ++i)
+		{
+			printf("%d ", copy3[i]);
+		}
+		printf("\n");
+		// Free the allocated memory
+		free(copy3);
+	}
+	else
+	{
+		printf("Test 3 failed: Memory allocation error\n");
+	}
 	return (0);
 }
