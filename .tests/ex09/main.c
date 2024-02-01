31c7dbd25770424167ed172b4ddea9acfa44fa06
flomero <49640251+flomero@users.noreply.github.com>
2024-02-01 09:52:16 +0100

    ex09 changes and test

--- .tests/ex09/main.c
+++ .tests/ex09/main.c
@@ -6,16 +6,21 @@
 /*   By: flfische <flfische@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/31 20:03:31 by flfische          #+#    #+#             */
-/*   Updated: 2024/01/31 20:03:40 by flfische         ###   ########.fr       */
+/*   Updated: 2024/02/01 09:16:31 by flfische         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-void	replace_space_with_tab(char *str);
+#include <stdio.h>
+
+char	*str_swap_halves(char *str);
 
 int	main(int argc, char **argv)
 {
+	char	*str;
+
 	if (argc != 2)
 		return (0);
-	replace_space_with_tab(argv[1]);
+	str = str_swap_halves(argv[1]);
+	printf("%s\n", str);
 	return (0);
 }
---
+++ .tests/ex09/test09.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+source .tests/compile_and_test.sh
+
+# Setup variables
+EXERCISE_DIR="code/ex09"
+EXERCISE_FILE="ex09.c"
+MAIN_FILE=".tests/ex09/main.c"
+EXECUTABLE="test_executable"
+EXPECTED_FILE=".tests/ex09/09.expected"
+OUTPUT_FILE="output.tmp"
+TEST_INPUTS=("HelloWorld" "BaumHaus" "1234567" "9" "" "123")
+CURRENT_EXERCISE_NUMBER="09"
+NEXT_EXERCISE_NUMBER="10"
+
+# Call functions
+compile_exercise "$EXERCISE_DIR" "$EXERCISE_FILE" "$MAIN_FILE" "$EXECUTABLE"
+run_tests "$EXECUTABLE" "$OUTPUT_FILE" "${TEST_INPUTS[@]}"
+validate_output "$EXPECTED_FILE" "$OUTPUT_FILE" "$CURRENT_EXERCISE_NUMBER"
+prepare_next_exercise "$NEXT_EXERCISE_NUMBER"
+cleanup "$EXECUTABLE" "$OUTPUT_FILE"
